6ff45052001bea1cb7eb85c150d9ecfd46bcdb77
tfsn <tfsn86@gmail.com>
2022-07-30 10:51:31 +0200

    minor layout changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,14 @@
 export const Home = () => {
   return (
     <>
-      <div className='flex flex-col md:flex-row container mx-auto px-8 md:pt-32'>
-        <div className='md:basis-1/3 md:pt-20 md:border-r-2 md:border-dotted md:border-black'>
+      <div className='flex flex-col md:flex-row container mx-auto px-8 md:pt-32 items-center'>
+        <div className='md:basis-1/3'>
           <h1 className='m-10 md:m-20 text-center'>About</h1>
         </div>
-        <div className='md:basis-2/3 md:w-32'>
+        <div className='md:basis-2/3 md:w-32 md:border-l-2 md:border-dotted md:border-black'>
           <p className='m-6 md:m-20'>
             I am a Business Analyst at the IT and Development Agency of the
-            Danish Ministry of Taxation. Outside work I am coding web
+            Danish Ministry of Taxation. Outside work I like to code web
             applications. Some of them are displayed on this site.
             <br />
             <br />
